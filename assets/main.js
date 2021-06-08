3a0ba912e1ed977a780036bd6543f6eb978c32b7
jheadlee <jheadlee@evolveip.net>
2021-06-08 16:29:28 -0400

    cat stuff

--- assets/main.js
+++ assets/main.js
@@ -6,6 +6,7 @@ var showFact = document.getElementById("show-fact");
 var CatFacts = ["The ancient Egyptians were the first to tame the cat (in about 3000 BC), and used them to control pests.",
                "Cats share 95.6% of their DNA with tigers.",
                "Cats are asleep for 70% of their lives.",
+               "Cats try to steal your soul while you sleep.",
                "Cat kidneys are super efficient, they can rehydrate by drinking seawater.",
                "Kittens who are taken along on short, trouble-free car trips to town tend to make good passengers when they get older. They get used to the sounds and motions of traveling and make less connection between the car and the visits to the vet."]
 
